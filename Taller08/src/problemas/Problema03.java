cd7c8453501bc353f0ffc4e80dbfd8c35af54cce
jhandry naula <jjnaula3@utpl.edu.ec>
2025-01-24 10:55:18 -0500

    cambios xd

--- Taller08/src/problemas/Problema03.java
+++ Taller08/src/problemas/Problema03.java
@@ -14,23 +14,22 @@ public class Problema03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Parte ascendente (de 1 a 5 estrellas)
+      
         for (int i = 1; i <= 5; i++) {
-            // Imprimir 'i' estrellas
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            // Salto de línea después de cada fila
+         
             System.out.println();
         }
 
-        // Parte descendente (de 4 a 1 estrella)
+       
         for (int i = 4; i >= 1; i--) {
-            // Imprimir 'i' estrellas
+         
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            // Salto de línea después de cada fila
+            
             System.out.println();
         }
     }
